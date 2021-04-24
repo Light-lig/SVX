fff92f5bd236e38d3d246dd0c06846890095260c
Light-lig <lightcaceres@gmail.com>
2021-04-24 15:15:12 -0600

    Estilos

--- SVX/App_Start/BundleConfig.cs
+++ SVX/App_Start/BundleConfig.cs
@@ -9,22 +9,21 @@ namespace SVX
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                    "~/assets/vendor_components/jquery/dist/jquery.min.js",
+                           "~/assets/vendor_components/jquery/dist/jquery.min.js",
+                           "~/assets/vendor_components/popper/dist/popper.min.js",
+                         "~/assets/vendor_components/bootstrap/dist/js/bootstrap.min.js",
+                          "~/assets/vendor_components/jquery-slimscroll/jquery.slimscroll.min.js",
+                           "~/assets/vendor_components/fastclick/lib/fastclick.js",
+                           "~/Scripts/js/template.js",
+                           "~/Scripts/js/demo.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
 
-            // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información. De este modo, estará
-            // para la producción, use la herramienta de compilación disponible en https://modernizr.com para seleccionar solo las pruebas que necesite.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/assets/").Include(
+                      "~/assets/vendor_components/bootstrap/dist/css/bootstrap.min.css",
+                      "~/Content/css/bootstrap-extend.css",
+                      "~/Content/css/master_style.css",
+                      "~/Content/css/skins/_all-skins.css"));
         }
     }
 }
